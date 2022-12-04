2a0da988e6ba79985334c6213f999c9ae5886fd3
sirrah23 <hseth93@gmail.com>
2022-12-04 02:40:04 -0500

    aoc 04.1

---
+++ 04/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("failed to open input file: %v", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	ans := 0
+	for scanner.Scan() {
+		pair1, pair2 := extractPairs(scanner.Text())
+		if (pair1.Low >= pair2.Low && pair1.High <= pair2.High) || (pair2.Low >= pair1.Low && pair2.High <= pair1.High) {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
+
+type pair struct {
+	Low  int
+	High int
+}
+
+func extractPairs(line string) (pair, pair) {
+	rawPairs := strings.Split(line, ",")
+	rawPair1, rawPair2 := rawPairs[0], rawPairs[1]
+	rawPair1Comps := strings.Split(rawPair1, "-")
+	rawPair2Comps := strings.Split(rawPair2, "-")
+	rawPair1Low, err := strconv.Atoi(rawPair1Comps[0])
+	if err != nil {
+		panic(err)
+	}
+	rawPair1High, err := strconv.Atoi(rawPair1Comps[1])
+	if err != nil {
+		panic(err)
+	}
+	rawPair2Low, err := strconv.Atoi(rawPair2Comps[0])
+	if err != nil {
+		panic(err)
+	}
+	rawPair2High, err := strconv.Atoi(rawPair2Comps[1])
+	if err != nil {
+		panic(err)
+	}
+	return pair{rawPair1Low, rawPair1High}, pair{rawPair2Low, rawPair2High}
+}
