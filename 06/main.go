b6b7e38df4c0a26a202198203912d8afbc647577
sirrah23 <hseth93@gmail.com>
2022-12-06 00:25:42 -0500

    06.2

--- 06/main.go
+++ 06/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	runTest()
+	runTestPart1()
+	runTestPart2()
 
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -20,7 +21,9 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 	scanner.Scan()
 	line := scanner.Text()
+
 	fmt.Println(markerSubroutine(line))
+	fmt.Println(messageSubroutine(line))
 }
 
 func markerSubroutine(input string) int {
@@ -40,7 +43,24 @@ func markerSubroutine(input string) int {
 	return -1
 }
 
-func runTest() {
+func messageSubroutine(input string) int {
+	for i := 0; i+14 < len(input); i++ {
+		currWindow := input[i : i+14]
+		repeat := false
+		for j, char := range currWindow {
+			if strings.Index(currWindow, string(char)) != j {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			return i + 14
+		}
+	}
+	return -1
+}
+
+func runTestPart1() {
 	for i, tc := range []struct {
 		TestInput      string
 		ExpectedMarker int
@@ -52,6 +72,25 @@ func runTest() {
 		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11},
 	} {
 		if out := markerSubroutine(tc.TestInput); out != tc.ExpectedMarker {
+			fmt.Printf("Expected %d but got %d for %s\n", tc.ExpectedMarker, out, tc.TestInput)
+		} else {
+			fmt.Printf("Pass test case %d\n", i)
+		}
+	}
+}
+
+func runTestPart2() {
+	for i, tc := range []struct {
+		TestInput      string
+		ExpectedMarker int
+	}{
+		{"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19},
+		{"bvwbjplbgvbhsrlpgdmjqwftvncz", 23},
+		{"nppdvjthqldpwncqszvftbrmjlhg", 23},
+		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29},
+		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26},
+	} {
+		if out := messageSubroutine(tc.TestInput); out != tc.ExpectedMarker {
 			fmt.Printf("Expected %d but got %d for %s\n", tc.ExpectedMarker, out, tc.TestInput)
 		} else {
 			fmt.Printf("Pass test case %d\n", i)
